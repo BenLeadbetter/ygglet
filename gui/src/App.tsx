037fa3d4979bdde1a2bbc5960aa81d7948f4e232
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2026-01-26 23:09:36 +0100

    feat: adds react gui and standalone target

---
+++ gui/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Welcome to Ygglet</h1>
+      <p>The extensible modular synthesizer</p>
+    </main>
+  );
+}
+
+export default App;
